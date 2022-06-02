2db72065934b617d7763b2a5b37759eed4732f6f
Dobiks <dobiks99@gmail.com>
2022-06-02 16:18:58 +0200

    ImageLoader class added

---
+++ ImageLoader.py
@@ -0,0 +1,47 @@
+import time
+import cv2 as cv,cv2
+import numpy as np
+import os
+
+SCALE = 100
+
+class ImageLoader:
+    def __init__(self):
+        self.img_list = self.load_images()
+
+    def load_images(self):
+        img_list = []
+        files = sorted(os.listdir("c6s1/frames"))            
+        for file in files:
+            if file.endswith(".jpg"):
+                img_color = cv.imread("c6s1/frames/" + file)
+                img_list.append((file, img_color))
+        return img_list
+    
+    def resize(self, image = None):
+        width = int(image.shape[1] * SCALE / 100)
+        height = int(image.shape[0] * SCALE / 100)
+        dim = (width, height)
+        return cv2.resize(image, dim, interpolation=cv2.INTER_AREA)
+
+    def drawBB(self,tup_img):
+        img = tup_img[1]
+        img_name = tup_img[0]
+        with open(f"c6s1/bboxes.txt", "r") as f:
+            lines = f.readlines()
+            for line in lines:
+                idx = lines.index(line)+1
+                if img_name in line and lines[idx].replace("\n","").isdigit():
+                    for i in range(int(lines[idx])):
+                        coords = lines[idx+i+1].split()
+                        cv.rectangle(img, (int(float(coords[0])), int(float(coords[1]))), (int(float(coords[0]))+int(float(coords[2])),int(float(coords[1]))+int(float(coords[3]))), (0, 255, 0), 2)
+        return img
+
+
+    def get_image(self, image_num):
+        img_color = self.img_list[image_num]
+        img_to_display = self.drawBB(img_color)
+        print(img_color[0])
+        resized = self.resize(image=img_to_display)
+        return resized
+
---
+++ main.py
@@ -0,0 +1,22 @@
+from ImageLoader import ImageLoader
+import time
+import cv2 as cv,cv2
+
+if __name__ == "__main__":
+    loader = ImageLoader()
+    cv.namedWindow('Image')
+    img_num = 0
+    while True:
+        img = loader.get_image(img_num)
+        cv.imshow('Image', img)
+        key_code = cv.waitKey(10)
+        if key_code == 27:
+            break
+        time.sleep(.01)
+        img_num += 1
+    cv.destroyAllWindows()
+
+        # elif key_code == ord('s'):
+        #     image_num += 1
+        # elif key_code == ord('a'):
+        #     image_num -= 1
